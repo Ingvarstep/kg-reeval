2e4aef94ca7eb9254a27f6031cbbf273c39a868b
Ihor Stepanov <64355919+Ingvarstep@users.noreply.github.com>
2021-07-13 10:54:43 -0400

    Update model.py

--- ConvE/model.py
+++ ConvE/model.py
@@ -24,9 +24,9 @@ class BaseModel(torch.nn.Module):
 		return stack_inp
 
 	def loss(self, pred, true_label=None):
-		pos_scr		= pred[:,0];
+		pos_scr		= pred[:,0]; 	  
 		neg_scr		= pred[:,1:]
-		label_pos	= true_label[0];
+		label_pos	= true_label[0]; 
 		label_neg	= true_label[1:]
 		loss		= self.bceloss(pred, true_label)
 		return loss
@@ -46,16 +46,14 @@ class ConvE(BaseModel):
 		flat_sz_h		= int(2*self.p.k_w) - self.p.ker_sz + 1
 		flat_sz_w		= self.p.k_h 	    - self.p.ker_sz + 1
 		self.flat_sz		= flat_sz_h*flat_sz_w*self.p.num_filt
-		# self.fc			= torch.nn.Linear(self.p.embed_dim, self.flat_sz)
-    		self.param_gen = Parameter(torch.rand(self.flat_sz, self.p.embed_dim))
+		self.fc			= torch.nn.Linear(self.flat_sz, self.p.embed_dim)
+
 		self.register_parameter('bias', Parameter(torch.zeros(self.p.num_ent)))
 
 	def forward(self, sub, rel):
 		sub_emb	= self.ent_embed(sub)
 		rel_emb	= self.rel_embed(rel)
-    		rr = torch.matmul(torch.unsqueeze(rel_emb, dim=-1), torch.transpose(torch.unsqueeze(rel_emb, dim=-1), 1, -1))
-		rg = torch.matmul(self.param_gen, rr)
-    		stk_inp	= self.concat(sub_emb, rel_emb, self.p.form)
+		stk_inp	= self.concat(sub_emb, rel_emb, self.p.form)
 		x	= self.bn0(stk_inp)
 		x	= self.input_drop(x)
 		x	= self.conv1(x)
@@ -63,12 +61,11 @@ class ConvE(BaseModel):
 		x	= F.relu(x)
 		x	= self.feature_drop(x)
 		x	= x.view(-1, self.flat_sz)
-		# x	= self.fc(x)
-    		x 	= torch.matmul(x.unsqueeze(dim=1), rg).squeeze()
+		x	= self.fc(x)
 		x	= self.hidden_drop(x)
 		x	= self.bn2(x)
 		x	= F.relu(x)
-
+		
 		x = torch.mm(x, self.ent_embed.weight.transpose(1,0))
 		x += self.bias.expand_as(x)
 
